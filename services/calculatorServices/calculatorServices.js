17dedd0c0efe31815810289e5a7626a73b5b12d4
Sofia Protsiv <prochivsofia@gmail.com>
2022-07-14 17:10:21 +0300

    removed unnecessary code terms

--- services/calculatorServices/calculatorServices.js
+++ services/calculatorServices/calculatorServices.js
@@ -4,10 +4,7 @@ const {
   getDailyLimit,
   getNotRecommendedCategories,
 } = require("../diaryServices");
-const {
-  newEntryValidation,
-  updateEntryValidation,
-} = require("../../middlewares");
+const { newEntryValidation } = require("../../middlewares");
 
 const publicCalc = async (body) => {
   const validated = newEntryValidation.validate(body);
@@ -30,70 +27,20 @@ const publicCalc = async (body) => {
 };
 
 const userCalc = async (body, userID) => {
-  const userFound = await User.findOne({ _id: userID });
+  const userFound = await User.findOne({ _id: userID }, { password: 0 });
   if (!userFound) {
     console.log("no such user");
   }
-  if (!userFound.dailyLimit) {
-    const target = await publicCalc(body);
-    userFound.userData.age = body.age;
-    userFound.userData.currentWeight = body.currentWeight;
-    userFound.userData.height = body.height;
-    userFound.userData.desiredWeight = body.desiredWeight;
-    userFound.userData.bloodType = body.bloodType;
-    userFound.userData.bloodType = target.dailyLimit;
-    userFound.userData.notAllowedProducts = target.notRecommendedCategories;
-
-    await userFound.save();
-    return target;
-  }
-  const validated = updateEntryValidation.validate(body);
-  if (validated.error) {
-    console.log("validation error");
-  }
-  const { currentWeight, height, age, desiredWeight, bloodType } = body;
-  const dailyLimit = getDailyLimit(
-    currentWeight,
-    height || userFound.userData.height,
-    age || userFound.userData.age,
-    desiredWeight
-  );
-
-  if (bloodType) {
-    const notRecommended = await Product.find(
-      {
-        ["groupBloodNotAllowed." + bloodType]: { $eq: true },
-      },
-      { categories: 1, _id: 0 }
-    );
-    const notRecommendedCategories =
-      getNotRecommendedCategories(notRecommended);
-
-    userFound.userData.age = body.age || userFound.userData.age;
-    userFound.userData.currentWeight = body.currentWeight;
-    userFound.userData.height = body.height || userFound.userData.height;
-    userFound.userData.desiredWeight = body.desiredWeight;
-    userFound.userData.bloodType = body.bloodType;
-    userFound.userData.bloodType = dailyLimit;
-    userFound.userData.notAllowedProducts = notRecommendedCategories;
-
-    await userFound.save();
-    return {
-      dailyLimit,
-      notRecommendedCategories,
-    };
-  }
-
+  const target = await publicCalc(body);
   userFound.userData.currentWeight = body.currentWeight;
-  userFound.userData.height = body.height || userFound.userData.height;
-  userFound.userData.age = body.age || userFound.userData.age;
+  userFound.userData.height = body.height;
+  userFound.userData.age = body.age;
   userFound.userData.desiredWeight = body.desiredWeight;
-  userFound.userData.dailyLimit = dailyLimit;
-
+  userFound.userData.bloodType = body.bloodType;
+  userFound.userData.dailyRate = target.dailyRate;
+  userFound.notRecommended = target.notRecommendedCategories;
   await userFound.save();
-  return {
-    dailyLimit,
-  };
+  return userFound;
 };
 
 module.exports = {
