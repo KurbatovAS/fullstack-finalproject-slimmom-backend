2c242fccffb7fe05cb7d0f3a1db2afff8234dd74
Andrey Kurbatov <nonamegod@gmail.com>
2022-07-21 17:36:34 +0300

    remove commented code from getProductsByBloodType.js

--- services/getProductsByBloodType.js
+++ services/getProductsByBloodType.js
@@ -4,14 +4,5 @@ const getProductsByBloodType = (products, bloodType) =>
       if (el.groupBloodNotAllowed[Number(bloodType)]) return arr;
     })
     .flatMap((el) => el.title.ua);
-// .flatMap((el) => el.categories)
-// .reduce((acc, el, ind, arr) => {
-//   arr.indexOf(el) === ind ? acc.push(el) : acc;
-//   return acc;
-// }, []);
-
-// const getProductsByBloodType = async (products, bloodType) => {
-//   return products.filter((el) => el.groupBloodNotAllowed[bloodType]);
-// };
 
 module.exports = { getProductsByBloodType };
